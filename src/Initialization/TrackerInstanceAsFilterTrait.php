ddbce9194084e83cf699605d47522b56fb0c1028
Bart Jaskulski <bjaskulski@protonmail.com>
2026-08-06 11:19:57 +0200

    feat: add support for tracker buckets per plugin author
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Initialization/TrackerInstanceAsFilterTrait.php
+++ src/Initialization/TrackerInstanceAsFilterTrait.php
@@ -11,8 +11,11 @@ use WPDesk\Tracker\OptInOptOut;
  * @package WPDesk\Plugin\Flow\Initialization\Simple\
  */
 trait TrackerInstanceAsFilterTrait {
-	/** @var \WPDesk_Tracker_Interface */
-	private static $tracker_instance;
+	/** @var \WPDesk_Tracker_Interface[] */
+	private static $tracker_instances = [];
+
+	/** @var string|null */
+	private $tracker_bucket;
 
 	/**
 	 * Returns filter action name for tracker instance
@@ -36,7 +39,28 @@ trait TrackerInstanceAsFilterTrait {
 	 * @return \WPDesk_Tracker_Interface
 	 */
 	private function get_tracker_instance() {
-		return apply_filters( $this->get_tracker_action_name(), null );
+		return apply_filters( $this->get_bucket_tracker_action_name(), null );
+	}
+
+	private function get_bucket_tracker_action_name() {
+		return $this->get_tracker_action_name() . '/' . $this->get_tracker_bucket();
+	}
+
+	private function get_tracker_bucket() {
+		if ( null !== $this->tracker_bucket ) {
+			return $this->tracker_bucket;
+		}
+
+		$plugin_data = get_file_data(
+			$this->plugin_info->get_plugin_file_name(),
+			[ 'Author' => 'Author' ]
+		);
+		$bucket      = sanitize_key( $plugin_data['Author'] ?? '' );
+		$bucket      = apply_filters( 'wpdesk/tracker/bucket/' . $this->plugin_info->get_plugin_slug(), $bucket );
+
+		$this->tracker_bucket = sanitize_key( $bucket ) ?: 'wpdesk';
+
+		return $this->tracker_bucket;
 	}
 
 	/**
@@ -47,21 +71,29 @@ trait TrackerInstanceAsFilterTrait {
 	private function prepare_tracker_action() {
 		class_exists( \WPDesk_Tracker_Factory::class ); //autoload this class
 
-		add_filter( $this->get_tracker_action_name(), function ( $tracker_instance ) {
+		$bucket = $this->get_tracker_bucket();
+		add_filter( $this->get_tracker_action_name() . '/' . $bucket, function ( $tracker_instance ) use ( $bucket ) {
 			if ( is_object( $tracker_instance ) ) {
 				return $tracker_instance;
 			}
-			if ( is_object( self::$tracker_instance ) ) {
-				return self::$tracker_instance;
+			if ( isset( self::$tracker_instances[ $bucket ] ) ) {
+				return self::$tracker_instances[ $bucket ];
 			}
 			if ( apply_filters( 'wpdesk_can_start_tracker', true, $this->plugin_info ) ) {
-				$tracker_factory        = new \WPDesk_Tracker_Factory_Prefixed();
-				self::$tracker_instance = $tracker_factory->create_tracker( basename( $this->plugin_info->get_plugin_file_name() ) );
+				$tracker_factory                    = new \WPDesk_Tracker_Factory_Prefixed();
+				self::$tracker_instances[ $bucket ] = $tracker_factory->create_tracker(
+					basename( $this->plugin_info->get_plugin_file_name() ),
+					$bucket
+				);
 
-				do_action( 'wpdesk_tracker_started', self::$tracker_instance, $this->plugin_info );
+				do_action( 'wpdesk_tracker_started', self::$tracker_instances[ $bucket ], $this->plugin_info );
 
-				return self::$tracker_instance;
+				return self::$tracker_instances[ $bucket ];
 			}
+		}, 10 - $this->get_tracker_version() );
+
+		add_filter( $this->get_tracker_action_name(), function ( $tracker_instance ) {
+			return is_object( $tracker_instance ) ? $tracker_instance : $this->get_tracker_instance();
 		}, 10 - $this->get_tracker_version() );
 	}
 
